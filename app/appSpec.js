d6348b57a9d7493860a49af5dd1d2311331851f3
jon-wade <jonwadeuk@gmail.com>
2016-05-04 10:40:45 +0100

    basic Angular app plumbed and working with karma test runner operational

---
+++ app/appSpec.js
@@ -0,0 +1,21 @@
+describe('test calendar components', function(){
+
+    beforeEach(module('calendarDemoApp'));
+
+    //beforeEach(module('template.html'));
+
+    beforeEach(inject(function(){
+
+
+
+    }));
+
+    it('should do something', function(){
+
+        //expect().toBe();
+
+
+    });
+
+
+});
