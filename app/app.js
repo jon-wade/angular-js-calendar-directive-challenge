db3c904a03a167a0af48905ca444e6d20fd40a42
jon-wade <jonwadeuk@gmail.com>
2016-05-05 03:55:43 +0100

    add in some initial unit tests and test calendar creation

--- app/app.js
+++ app/app.js
@@ -2,6 +2,55 @@ var app = angular.module('calendarDemoApp', []);
 
 app.controller('home', function($scope){
 
+    $scope.monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    $scope.yearArray = [];
+
+    //get current date
+    var date = new Date();
+    //clean up the date format using supplied prepareDate() method
+    var preparedDateObject = CalendarRange.prepareDate(date);
+    console.log(preparedDateObject);
+
+    //set the current month into $scope variables that can be used to default the dropdown list.
+    $scope.currentMonth = $scope.monthArray[preparedDateObject.month];
+    //set the current year into $scope variables that can be used to default the dropdown list.
+    $scope.currentYear = preparedDateObject.year;
+
+    //populate yearArray with previous 20, and future 20 years for creating options for dropdown list
+    for (var i=preparedDateObject.year-20; i<=preparedDateObject.year+20; i++){
+        $scope.yearArray.push(i);
+    }
+
+    //test date creation
+    console.log(new Date(2016, 0));
+    $scope.range = CalendarRange.getMonthlyRange(new Date(2016,0));
+    console.log($scope.range);
+
+
+    //create an array to hold the number of day boxes for each month and populate
+    $scope.dateArray = [];
+    for(var i=1; i<=$scope.range.days.length; i++){
+        $scope.dateArray.push(i);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //grabs the values from the drop-down list once the second drop down list has been selected and stores in variables
     $scope.submit = function(){
@@ -14,14 +63,16 @@ app.controller('home', function($scope){
         $scope.debug1 = 'Selected month: ' + selectedMonth + ' Selected Year: ' + selectedYear;
     };
 
-    //get current date, prepare and return monthly range
-    var date = new Date();
 
-    var preparedDateObject = CalendarRange.prepareDate(date);
-    console.log(preparedDateObject);
 
-    var rangeDateObject = CalendarRange.getMonthlyRange(date);
-    console.log(rangeDateObject);
+
+
+
+
+    //var rangeDateObject = CalendarRange.getMonthlyRange(date);
+    //console.log(rangeDateObject);
+
+
 
 
 
--- app/appSpec.js
+++ app/appSpec.js
@@ -10,18 +10,27 @@ describe('test calendar components', function(){
         scope = $rootScope.$new();
         controller = $controller('home', {
             $scope: scope});
-
     }));
 
     it('should submit the month and year when the second drop down is selected', function(){
         scope.month = 1;
         scope.year = 2016;
-
         scope.submit();
-
         expect(scope.debug1).toBe('Selected month: ' + scope.month + ' Selected Year: ' + scope.year);
-
     });
+
+    it('should set the $scope.currentMonth to current month in written form', function(){
+        expect(scope.currentMonth).toBe('May');
+    });
+
+    it('should set the $scope.currentYear to current year in numeric form', function(){
+        expect(scope.currentYear).toBe(2016);
+    });
+
+    it('should create a $scope.yearArray beginning in 1996 and ending in 2036', function(){
+        expect(scope.yearArray[0]).toBe(1996);
+        expect(scope.yearArray[scope.yearArray.length-1]).toBe(2036);
+    })
 
 
 });
