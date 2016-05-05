818a2b72a5d90f9fd9db27f6398fa94d5ad4fdfd
jon-wade <jonwadeuk@gmail.com>
2016-05-05 06:31:13 +0100

    add in factory method to populate dropdown, shading for weekends

--- app/app.js
+++ app/app.js
@@ -1,37 +1,57 @@
 var app = angular.module('calendarDemoApp', []);
 
-app.controller('home', function($scope){
+app.factory('dropdown', function(){
+    return {
+        'populate': function(){
+            var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            var yearArray = [];
 
-    $scope.monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    $scope.yearArray = [];
+            //get current date
+            var date = new Date();
+            //clean up the date format using supplied prepareDate() method
+            var preparedDateObject = CalendarRange.prepareDate(date);
 
-    //get current date
-    var date = new Date();
-    //clean up the date format using supplied prepareDate() method
-    var preparedDateObject = CalendarRange.prepareDate(date);
-    console.log(preparedDateObject);
+            //set the current month into $scope variables that can be used to default the dropdown list.
+            var currentMonth = monthArray[preparedDateObject.month];
+            //set the current year into $scope variables that can be used to default the dropdown list.
+            var currentYear = preparedDateObject.year;
 
-    //set the current month into $scope variables that can be used to default the dropdown list.
-    $scope.currentMonth = $scope.monthArray[preparedDateObject.month];
-    //set the current year into $scope variables that can be used to default the dropdown list.
-    $scope.currentYear = preparedDateObject.year;
+            //populate yearArray with previous 20, and future 20 years for creating options for dropdown list
+            for (var i=preparedDateObject.year-20; i<=preparedDateObject.year+20; i++){
+                yearArray.push(i);
+            }
 
-    //populate yearArray with previous 20, and future 20 years for creating options for dropdown list
-    for (var i=preparedDateObject.year-20; i<=preparedDateObject.year+20; i++){
-        $scope.yearArray.push(i);
+            return {
+                date: date,
+                monthArray: monthArray,
+                yearArray: yearArray,
+                currentMonth: currentMonth,
+                currentYear: currentYear
+            };
+        }
     }
+});
 
-    //test date creation
-    console.log(new Date(2016, 0));
-    $scope.range = CalendarRange.getMonthlyRange(new Date(2016,0));
+app.controller('home', ['$scope', 'dropdown', function($scope, dropdown){
+
+    //populate the dropdown lists with the month and year data, and set to current month and year
+    $scope.monthArray = dropdown.populate().monthArray;
+    $scope.yearArray = dropdown.populate().yearArray;
+    $scope.currentMonth = dropdown.populate().currentMonth;
+    $scope.currentYear = dropdown.populate().currentYear;
+
+    //grab the current date
+    var date = dropdown.populate().date;
+
+    //get the range object based on the current date
+    $scope.range = CalendarRange.getMonthlyRange(new Date(date));
+
     console.log($scope.range);
 
-
-    //create an array to hold the number of day boxes for each month and populate
-    $scope.dateArray = [];
-    for(var i=1; i<=$scope.range.days.length; i++){
-        $scope.dateArray.push(i);
-    }
+    //$scope.dateArray = [];
+    //for(var i=1; i<=$scope.range.days.length; i++){
+    //    $scope.dateArray.push(i);
+    //}
 
     //grabs the values from the drop-down list once the second drop down list has been selected and stores in variables
     $scope.submit = function(){
@@ -44,17 +64,19 @@ app.controller('home', function($scope){
         $scope.debug1 = 'Selected month: ' + selectedMonth + ' Selected Year: ' + selectedYear;
     };
 
-});
+}]);
 
-app.directive('calendarDay', function(){
+app.directive('calendarDay', ['dropdown', function(dropdown){
     return{
         restrict: 'E',
-        transclude: false,
+        replace: true,
         templateUrl: 'template.html',
         scope: true,
         link: function(scope, element, attrs){
 
         }
     }
-});
+}]);
+
+
 
