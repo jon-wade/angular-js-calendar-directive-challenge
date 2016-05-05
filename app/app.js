bdb570fa305761de86fbc389a3907164b9318c13
jon-wade <jonwadeuk@gmail.com>
2016-05-05 02:03:31 +0100

    add dropdowns, data submission and experiment with predefined methods

--- app/app.js
+++ app/app.js
@@ -1,3 +1,29 @@
-angular.module('calendarDemoApp', []);
+var app = angular.module('calendarDemoApp', []);
 
-// your controller and directive code go here
+app.controller('home', function($scope){
+
+
+    //grabs the values from the drop-down list once the second drop down list has been selected and stores in variables
+    $scope.submit = function(){
+        console.log('month and year submitted');
+        var selectedMonth = $scope.month;
+        var selectedYear = $scope.year;
+        console.log('Selected month: ', selectedMonth);
+        console.log('Selected year: ', selectedYear);
+
+        $scope.debug1 = 'Selected month: ' + selectedMonth + ' Selected Year: ' + selectedYear;
+    };
+
+    //get current date, prepare and return monthly range
+    var date = new Date();
+
+    var preparedDateObject = CalendarRange.prepareDate(date);
+    console.log(preparedDateObject);
+
+    var rangeDateObject = CalendarRange.getMonthlyRange(date);
+    console.log(rangeDateObject);
+
+
+
+});
+
--- app/appSpec.js
+++ app/appSpec.js
@@ -1,19 +1,25 @@
 describe('test calendar components', function(){
 
+    var scope, controller;
+
     beforeEach(module('calendarDemoApp'));
 
     //beforeEach(module('template.html'));
 
-    beforeEach(inject(function(){
-
-
+    beforeEach(inject(function($rootScope, $controller){
+        scope = $rootScope.$new();
+        controller = $controller('home', {
+            $scope: scope});
 
     }));
 
-    it('should do something', function(){
+    it('should submit the month and year when the second drop down is selected', function(){
+        scope.month = 1;
+        scope.year = 2016;
 
-        //expect().toBe();
+        scope.submit();
 
+        expect(scope.debug1).toBe('Selected month: ' + scope.month + ' Selected Year: ' + scope.year);
 
     });
 
